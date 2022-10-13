74ee9cf62e3e748f880aedff2a55838233540ae1
rioperezg <113980673+rioperezg@users.noreply.github.com>
2022-10-13 12:27:58 +0200

    sdfgh

--- main.py
+++ main.py
@@ -3,3 +3,12 @@ numero = random,random.randint(0, 100)
 print ("introduzca un numero a adivinar")
 while True:
     numero = input("introduzca un numero entre 0 y 99")
+
+    try:
+        numero = int(numero)
+    except:
+        pass
+    else:
+        if 0 <= numero <= 99:
+
+           break

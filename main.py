4461dff87fff96c855d83bde5875534ea8f0cf70
rioperezg <113980673+rioperezg@users.noreply.github.com>
2022-10-13 12:30:03 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import random
 numero = random,random.randint(0, 100)
-print ("introduzca un numero a adivinar")
+print ("introduzca un numero a adivinaffffffr")
 while True:
     numero = input("introduzca un numero entre 0 y 99")
 

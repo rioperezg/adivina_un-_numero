285a312b571faa8c10d159bc89abd0c12da4c91f
rioperezg <113980673+rioperezg@users.noreply.github.com>
2022-10-13 12:18:53 +0200

    d.drawio y main deleted

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
 import random
 numero = random,random.randint(0, 100)
-print (numero)
+print ("introduzca un numero a adivinar")
+while True:
+    numero = input("introduzca un numero entre 0 y 99")

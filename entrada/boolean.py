fb3c6366a940f39a0e299508190657fc6681d0da
rioperezg <113980673+rioperezg@users.noreply.github.com>
2022-10-17 13:35:42 +0200

    juego.commit

---
+++ entrada/boolean.py
@@ -0,0 +1,17 @@
+SI = ("s", "si", "y", "yes", "1")
+VERDADERO = ("v", "verdadero", "t", "true", "1")
+
+
+def pedir_entrada_si_o_no(invite):
+    """Por defecto, toda respuesta no comprendida será NO"""
+    try:
+        return input(invite).lower() in SI
+    except:
+        return False
+
+def pedir_entrada_verdadero_o_falso(invite):
+    """Por defecto, toda respuesta no comprendida será FALSO"""
+    try:
+        return input(invite).lower() in VERDADERO
+    except:
+        return False

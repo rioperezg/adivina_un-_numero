64ec498e6bf742015fde3a2797995e6b7d863bc9
rioperezg <rioperezg@gmail.com>
2025-03-12 16:34:27 +0100

    ciertos cambios de import

--- juego.py
+++ juego.py
@@ -1,10 +1,6 @@
 
-from entrada import (
-    pedir_entrada_numero,
-    pedir_entrada_numero_delimitado,
-    pedir_entrada_si_o_no,
-)
-
+from num import pedir_entrada_numero, pedir_entrada_numero_delimitado
+from boolean import pedir_entrada_si_o_no
 
 def jugar_una_vez(numero, minimo, maximo):
     intento = pedir_entrada_numero_delimitado("Adivine el numero", minimo, maximo)
